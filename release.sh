a05a9c48d50fbff1d805ed33f414bb5faea8e591
John Anderson <sontek@gmail.com>
2015-07-08 11:33:54 -0700

    use pypi

--- release.sh
+++ release.sh
@@ -6,18 +6,11 @@
 PKG_NAME=$(python setup.py --name)
 PKG_VERSION=$(cat VERSION)
 PKG_FILE=${PKG_NAME}-${PKG_VERSION}.tar.gz
-CURL_URL=http://packages/index/${PKG_FILE}
 CURRENT_BRANCH=$(git branch|awk -F' ' '{ print $2}')
-echo "Checking if ${PKG_NAME}-${PKG_VERSION} already exists..."
-curl --silent --head --fail -o /dev/null ${CURL_URL}
-if [ $? -eq 0 ]; then
-  echo "${PKG_NAME}-${PKG_VERSION} already exists, aborting."
-  exit 1
-fi
 
 pip install setuptools-git
 git tag -am "release ${PKG_VERSION}" ${PKG_VERSION}
 git push --tags
 git checkout ${PKG_VERSION}
-python setup.py sdist upload -r http://packages
+python setup.py sdist upload
 git checkout ${CURRENT_BRANCH}
